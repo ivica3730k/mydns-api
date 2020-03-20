a314332b1c56be054801bca0fc4f91ab5ab3ffd4
IcE <ivica-matic@outlook.com>
2020-03-20 21:46:52 +0000

    Implemented ability to add zone and update record

--- models/model_soa.php
+++ models/model_soa.php
@@ -2,14 +2,14 @@
 
 require "dbconnection.php";
 
-function addSoa($_apikey, $_origin, $_optimiseForDdns)
+function addSoa($_apikey, $_zone, $_optimiseForDdns)
 {
     $conn = connectToDatabase();
     if ($conn->connect_errno) echo "Unable to access database";
     $apikey = $conn->real_escape_string($_apikey);
-    $origin = $conn->real_escape_string($_origin);
+    $zone = $conn->real_escape_string($_zone);
     $optimiseForDdns = $conn->real_escape_string($_optimiseForDdns);
-    $sql = "CALL addSoa('$apikey','$origin','$optimiseForDdns')";
+    $sql = "CALL addSoa('$apikey','$zone','$optimiseForDdns')";
     $result = $conn->query($sql);
     closeDatabase($conn);
     if ($result->num_rows > 0) {
